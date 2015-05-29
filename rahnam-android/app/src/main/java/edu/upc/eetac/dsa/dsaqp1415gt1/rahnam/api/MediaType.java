f7abbbdde884e08e6d8775e3416fbde87ba7e5ed
cristina sanchez <cristina.s.narbon@gmail.com>
2015-05-29 16:05:20 +0200

    Creacion proyecto Android

---
+++ rahnam-android/app/src/main/java/edu/upc/eetac/dsa/dsaqp1415gt1/rahnam/api/MediaType.java
@@ -0,0 +1,6 @@
+package edu.upc.eetac.dsa.dsaqp1415gt1.rahnam.api;
+
+public interface MediaType {
+    public final static String RAHNAM_API_PHOTO = "application/vnd.rahnam.api.photo+json";
+    public final static String RAHNAM_API_PHOTO_COLLECTION = "application/vnd.rahnam.api.photo.collection+json";
+}
---
+++ rahnam-android/app/src/main/java/edu/upc/eetac/dsa/dsaqp1415gt1/rahnam/api/RahnamAPI.java
@@ -0,0 +1,7 @@
+package edu.upc.eetac.dsa.dsaqp1415gt1.rahnam.api;
+
+/**
+ * Created by Cristina on 29/05/2015.
+ */
+public class RahnamAPI {
+}
